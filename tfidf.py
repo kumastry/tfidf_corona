dff04f4752fd825d25836661be8cb5b980dbf60c
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-10-02 01:33:10 +0900

    fixed

--- tfidf.py
+++ tfidf.py
@@ -1,6 +1,18 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pickle
+import json
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
 
 data = []
 with open('data.txt', 'rb') as f:
@@ -26,7 +38,15 @@ feature_words = [feature_names[doc[:n]] for doc in index]
 #print(len(feature_names))
 #print(feature_words)
 
+json_data = []
+
 for i,j in zip(feature_words, locals):
     print(j)
     print(i)
     print('')
+
+    json_data.append({"local":j, "top":i})
+
+
+with open('tfidf.json',  mode='wt', encoding='utf-8') as f:
+    json.dump(json_data, f, ensure_ascii=False, cls = MyEncoder,indent=2)
